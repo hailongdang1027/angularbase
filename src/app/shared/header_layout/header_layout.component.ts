ab75d9d91cb42a9176e1cbb7e10d41d363039eda
hailongdang <hailong27102000@gmail.com>
2024-10-30 23:14:35 +0700

    first commit

---
+++ src/app/shared/header_layout/header_layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'header-layout',
+    standalone: true,
+    templateUrl: './header_layout.component.html',
+    styleUrl: './header_layout.component.css'
+})
+
+export class HeaderLayoutComponent{
+
+}
